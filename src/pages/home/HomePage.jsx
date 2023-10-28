f9da1c0ad66fdd523e91ee188c23cc251e82ad52
Ackeloo1 <axel.mattias.nyberg@gmail.com>
2023-10-28 02:01:38 +0200

    Add images

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,8 @@
 import { NavLink } from "react-router-dom";
 import "./HomePage.css";
+import abilityImage from '../../images/ability.png';
+import loreImage from '../../images/lore.png';
+import splashImage from '../../images/splash.png';
 
 const HomePage = () =>{
     return(
@@ -7,9 +10,15 @@ const HomePage = () =>{
             <div className="home-window">
                 <div className="home-title">Choose blyad</div>
                 <div className="home-items">
-                    <NavLink to='/ability' className="home-item">Ability</NavLink>
-                    <NavLink to='/lore' className="home-item">Lore</NavLink>
-                    <NavLink to='/splash' className="home-item">Splash</NavLink>
+                    <NavLink to='/ability' className="home-item">
+                        <img src={abilityImage} alt="ability" />
+                    </NavLink>
+                    <NavLink to='/lore' className="home-item">
+                        <img src={loreImage} alt="lore" />
+                    </NavLink>
+                    <NavLink to='/splash' className="home-item">
+                        <img src={splashImage} alt="splash" />
+                    </NavLink>
                 </div>
             </div>
         </div>
