9284644c29085a7c600eefbcb2fab9d06be29446
Ackeloo1 <axel.mattias.nyberg@gmail.com>
2023-10-30 15:11:13 +0100

    add ability and home tests

---
+++ src/pages/home/HomePage.test.js
@@ -0,0 +1,91 @@
+import { React } from 'react';
+import HomePage from './HomePage';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { render, fireEvent, screen } from '@testing-library/react';
+
+it('HomePage renders without crashing', () => {
+    render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+});
+
+it('Matches previous snapshot', () => {
+    const {asFragment} = render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+});
+
+it('contains a title', () => {
+    const {getByText} = render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+    expect(getByText('Choose blyad')).toBeInTheDocument();
+});
+
+it('contains all nav-links', () => {
+    const {getAllByRole} = render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+    expect(getAllByRole("link")).toHaveLength(3);
+});
+
+it('has the correct CSS classes', () => {
+    const {container} = render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+    expect(container.querySelector(".home-container")).toBeInTheDocument();
+    expect(container.querySelector(".home-window")).toBeInTheDocument();
+});
+
+it("navigates to 'ability' page", () => {
+    render(
+      <BrowserRouter initialEntries={["/"]}>
+        <HomePage />
+      </BrowserRouter>
+    );
+  
+    const abilityLink = screen.getByAltText("ability");
+
+    fireEvent.click(abilityLink);
+    
+    expect(window.location.pathname).toBe("/ability");
+  });
+
+it("navigates to 'lore' page", () => {
+    render(
+        <BrowserRouter>
+            <HomePage />
+        </BrowserRouter>
+    )
+
+    const loreLink = screen.getByAltText("lore");
+
+    fireEvent.click(loreLink);
+    
+    expect(window.location.pathname).toBe("/lore");
+});
+
+it("navigates to 'splash' page", () => {
+    render(
+        <BrowserRouter>
+            <HomePage />
+        </BrowserRouter>
+    )
+
+    const splashLink = screen.getByAltText("splash");
+
+    fireEvent.click(splashLink);
+    
+    expect(window.location.pathname).toBe("/splash");
+});
--- src/pages/login/LoginPage.test.js
+++ src/pages/login/LoginPage.test.js
@@ -6,4 +6,5 @@ test('LoginPage Renders all elements on the page', () => {
   const {getByText} = render(<LoginPage />);
   expect(getByText('Användarnamn')).toBeInTheDocument();
   expect(getByText('Lösenord')).toBeInTheDocument();
-})
+  expect(getByText('Logga in')).toBeInTheDocument();
+});
