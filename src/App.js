18a45ea64f43b0b42fa60d1dc3a3760ebf198f37
Ackeloo1 <axel.mattias.nyberg@gmail.com>
2023-11-06 11:42:12 +0100

    Add register

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Routing from './pages/Routing';
 import NavBarPartial from './partials/nav/NavBarPartial';
 import {ChampionNamesProvider} from './contexts/ChampionNamesContext';
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
   return (
     <>
+    <UserProvider>
     <ChampionNamesProvider>
       <NavBarPartial/>
       <Routing />
     </ChampionNamesProvider>
+    </UserProvider>
     </>
   );
 }
--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,16 +1,99 @@
 import "./RegisterPage.css";
 import AccountButton from "../../partials/buttons/AccountButton";
 import InputBox from "../../partials/inputs/InputBox";
+import { useRef, useState, useContext } from "react";
+import { IsValidUsername, isValidPassword } from "../../helpers/ValidateInput";
+import { UserContext } from "../../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const RegisterPage = () => {
+
+    const navigation = useNavigate();
+    const {userUpdate, user} = useContext(UserContext);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    let usernameRef = useRef();
+    let passwordRef = useRef();
+
+    const [rememberMe, setRememberMe] = useState(false);
+    const rememberMeHandler = () => {
+        setRememberMe(!rememberMe);
+    }
+
+    const register = async () => {
+        const username = usernameRef.current.value;
+        const password = passwordRef.current.value;
+
+        if(!IsValidUsername(username)){
+            setErrorMessage("Invalid Username");
+            return;
+        }
+        if(!isValidPassword(password)){
+            setErrorMessage("Invalid Password");
+            return;
+        }
+
+        try{
+            let response;
+            let result;
+
+            response = await fetch(/*"https://localhost/api-endpoint"*/"", {
+                method: "post",
+                headers: {
+                    'Accept' : 'application/json',
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            });
+
+            if(response.ok !== true){
+                if(response.status === 409){
+                    setErrorMessage("Username already exists");
+                    return;
+                }
+
+                return;
+            }
+
+            result = await response.json();
+
+            setErrorMessage("success");
+
+            const token = result.jwt;
+            const decoded = jwtDecode(token);
+
+            let userBuilder = {
+                jwt: result.jwt,
+                username: decoded.username,
+                type : decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"],
+                rememberMe: rememberMe
+            };
+
+            userUpdate(userBuilder);
+
+            navigation("/");
+        }
+        catch(error){
+            console.error(error);
+            setErrorMessage("Something went wrong");
+            return;
+        }
+    }
+
     return(
     
     <div className="register-container">
         <div className="register-window">
             <div className="register-credentials">
-                <InputBox labelText="Användarnamn"/>
-                <InputBox labelText="Lösenord" />
-                <AccountButton text="Registrera"/>
+                <InputBox labelText="Användarnamn" ref={usernameRef} />
+                <InputBox labelText="Lösenord" ref={passwordRef} />
+                <p>Remember Me</p>
+                <input type="checkbox" onChange={rememberMeHandler} checked={rememberMe}/>
+                <div className="error-container">
+                    <p>{errorMessage == null ? "" : errorMessage}</p>
+                </div>
+                <AccountButton text="Registrera" onClick={() => register()}/>
             </div>
             
         </div>
