af9801c7647262b31bf2d9fd8fde974a50e8e2fd
Ackeloo1 <axel.mattias.nyberg@gmail.com>
2023-11-08 23:02:34 +0100

    Add fix

--- src/pages/home/HomePage.test.js
+++ src/pages/home/HomePage.test.js
@@ -89,3 +89,45 @@ it("navigates to 'splash' page", () => {
     
     expect(window.location.pathname).toBe("/splash");
 });
+
+it('does not contain title when there is no data', () => {
+    // You would have to mock the scenario where no data is passed to the HomePage
+    const {queryByText} = render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+    expect(queryByText('Choose blyad')).not.toBeInTheDocument();
+});
+
+it('does not show incorrect number of nav-links', () => {
+    // Mock the scenario where links are not as expected
+    const {queryAllByRole} = render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+    expect(queryAllByRole("link")).not.toHaveLength(4); // Assuming 3 is the correct number
+});
+
+it("does not navigate to an incorrect page when 'ability' is clicked", () => {
+    render(
+        <BrowserRouter>
+        <HomePage />
+        </BrowserRouter>
+    );
+
+    const abilityLink = screen.getByAltText("ability");
+    fireEvent.click(abilityLink);
+
+    expect(window.location.pathname).not.toBe("/incorrect-path");
+});
+
+it('does not show error message during normal operation', () => {
+    const {queryByText} = render(
+        <MemoryRouter>
+            <HomePage />
+        </MemoryRouter>
+    );
+    expect(queryByText('Error loading page')).not.toBeInTheDocument();
+});
